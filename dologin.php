bf5d0fa6a386908097e2ce510f48ec23b906fef9
Shreyansh Jain <shreyansh_k@live.com>
2016-10-15 23:00:38 +0530

    store session username

--- dologin.php
+++ dologin.php
@@ -1,22 +1,23 @@
 <?php
   session_start();
-  $uname = $_POST['uname'];
+  $username = $_POST['username'];
   $passhash = md5($_POST['password']);
-  //check username or email
   $userdb = new PDO('sqlite:/srv/http/iwp_project/users.sqlite');
-  $query1 = "select * from users where username = '".$uname."'";
-  $query2 = "select * from users where email = '".$uname."'";
+  $query1 = "select * from users where username = '".$username."'";
+  $query2 = "select * from users where email = '".$username."'";
   $res1 = $userdb->query($query1);
   $res2 = $userdb->query($query2);
   $validid = 0;
   foreach ($res1 as $row) {
     if ($row['passhash'] == $passhash) {
       $validid = 1;
+      $_SESSION['username'] = $row['username'];
     }
   }
   foreach ($res2 as $row) {
     if ($row['passhash'] == $passhash) {
       $validid = 1;
+      $_SESSION['username'] = $row['username'];
     }
   }
   if ($validid) { //check username & pass
