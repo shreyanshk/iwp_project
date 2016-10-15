a42879ccbfa9690a69f017609dcfd889d6401f9e
Shreyansh Jain <shreyansh_k@live.com>
2016-10-15 23:40:39 +0530

    fixed goof with dologin.php

--- dologin.php
+++ dologin.php
@@ -1,30 +1,37 @@
 <?php
   session_start();
-  $username = $_POST['username'];
-  $passhash = md5($_POST['password']);
-  $userdb = new PDO('sqlite:/srv/http/iwp_project/users.sqlite');
-  $query1 = "select * from users where username = '".$username."'";
-  $query2 = "select * from users where email = '".$username."'";
-  $res1 = $userdb->query($query1);
-  $res2 = $userdb->query($query2);
-  $validid = 0;
-  foreach ($res1 as $row) {
-    if ($row['passhash'] == $passhash) {
-      $validid = 1;
-      $_SESSION['username'] = $row['username'];
+  if (isset($_POST['username']) & isset($_POST['password']) & (!isset($_SESSION['username']))) {
+    $username = $_POST['username'];
+    $passhash = md5($_POST['password']);
+    $userdb = new PDO('sqlite:/srv/http/iwp_project/users.sqlite');
+    $query1 = "select * from users where username = '".$username."'";
+    $query2 = "select * from users where email = '".$username."'";
+    $res1 = $userdb->query($query1);
+    $res2 = $userdb->query($query2);
+    $validid = 0;
+    foreach ($res1 as $row) {
+      if ($row['passhash'] == $passhash) {
+        $validid = 1;
+        $_SESSION['username'] = $row['username'];
+      }
     }
-  }
-  foreach ($res2 as $row) {
-    if ($row['passhash'] == $passhash) {
-      $validid = 1;
-      $_SESSION['username'] = $row['username'];
+    foreach ($res2 as $row) {
+      if ($row['passhash'] == $passhash) {
+        $validid = 1;
+        $_SESSION['username'] = $row['username'];
+      }
     }
-  }
-  if ($validid) {
-    //echo "You have been logged in!";
+    if ($validid) {
+      //echo "You have been logged in!";
+      header('Location: home.php');
+    } else {
+      echo "Invalid userid password combo";
+      session_destroy();
+    }
+  } else if (isset($_SESSION['username'])) {
     header('Location: home.php');
   } else {
-    echo "Invalid userid password combo";
+    header('Location: login.php');
     session_destroy();
   }
 ?>
