f6e995d19f4a42a333de9aeacaf56324c54821db
Shreyansh Jain <shreyansh_k@live.com>
2016-10-16 15:00:24 +0530

    barebone signup page

--- signup.php
+++ signup.php
@@ -16,8 +16,42 @@ if (isset($_SESSION['username'])) {
     <div id='topbar'>
       <p id='topbarmid'>CodeDev SignUp</p>
     </div>
-    <div>
+    <div id='signupform'>
       <form action="dosignup.php" method="POST">
-        
+        <p>
+          First name<br>
+          <input type='text' id='fname'>
+        </p>
+        <p>
+          Last name<br>
+          <input type='text' id='lname'>
+        </p>
+        <p>
+          Preferred username<br>
+          <input type='text' id='username'>
+        </p>
+        <p>
+          Password<br>
+          <input type='password' id='password'>
+        </p>
+        <p>
+          Retype password<br>
+          <input type='text' id='cnfpassword'>
+        </p>
+        <p>
+          E-Mail address<br>
+          <input type='text' id='email'>
+        </p>
+        <p>
+          Your occupation<br>
+          <select id='occupation'>
+            <option value='student'>Student</option>
+            <option value='teacher'>Teacher</option>
+          </select>
+        </p>
+        <br>
+        <input type='submit' value='Sign Up'>
+      </form>
+    </div>
   </body>
 </html>
