c26f5957a2c0314cc7ac709656f28047060a50c0
Shreyansh Jain <shreyansh_k@live.com>
2016-10-20 17:47:09 +0530

    Fix login not redirecting correctly

--- dologin.php
+++ dologin.php
@@ -9,21 +9,28 @@
     $res1 = $userdb->query($query1);
     $res2 = $userdb->query($query2);
     $validid = 0;
+    $validcombo = 0;
     foreach ($res1 as $row) {
-      if ($row['passhash'] == $passhash) {
-        $validid = 1;
+      if ($row['passhash'] == $passhash && $row['username'] == $username) {
+        $validcombo = 1;
         $_SESSION['username'] = $row['username'];
+      } else if ($row['username'] == $username) {
+        $validid = 1;
       }
     }
     foreach ($res2 as $row) {
-      if ($row['passhash'] == $passhash) {
-        $validid = 1;
+      if ($row['passhash'] == $passhash && $row['username'] == $username) {
+        $validcombo = 1;
         $_SESSION['username'] = $row['username'];
+      } else if ($row['username'] == $username) {
+        $validid = 1;
       }
     }
-    if ($validid) {
+    if ($validcombo) {
       //echo "You have been logged in!";
       header('Location: home.php');
+    } else if ($validid) {
+      echo "Invalid password. Please try to login again.";
     } else {
       $redir = "signup.php?username=".$username;
       header("Location: $redir");
